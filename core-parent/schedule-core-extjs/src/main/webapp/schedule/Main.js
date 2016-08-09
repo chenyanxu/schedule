acecc33905ccf31e2749de20f91d657441c5cff2
sunlingfeng <minikiller@qq.com>
2016-08-09 15:05:03 +0800

    add schedule core extjs module

---
+++ core-parent/schedule-core-extjs/src/main/webapp/schedule/Main.js
@@ -0,0 +1,8 @@
+//todo 定义系统应用页面
+
+Ext.define('kalix.schedule.Main', {
+    extend: 'Ext.container.Container',
+    style: {
+        background: '#fff url(' + CONFIG.restRoot + '/schedule/resources/images/main.png) 100% 100% no-repeat'
+    }
+});
