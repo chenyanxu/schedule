b5f3abe4ff10f4656ec033dcf1dcc4efdd4540a9
paning <pwcn_78@163.com>
2016-08-12 14:35:03 +0800

    个人计划查询开发

---
+++ plan-parent/personalplan-parent/schedule-plan-personalplan-extjs/src/main/webapp/personalplan/controller/PersonalPlanSearchController.js
@@ -0,0 +1,14 @@
+Ext.define('kalix.plan.personalplan.controller.PersonalPlanSearchController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.personalplansearchController',
+    onItemClick: function (view, record, item, index, e) {
+        //var dutyGrid=this.getView().items.getAt(1);
+        //
+        //dutyGrid.columns[4].tpl.html=record.data.name;
+        //
+        //var store =dutyGrid.store;
+        //
+        //store.proxy.url=CONFIG.restRoot +'/camel/rest/orgs/'+record.data.id+'/dutys';
+        //store.reload();
+    }
+});
