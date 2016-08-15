2a84e318a04a1c631afb6dd72ec1f55de2caea14
paning <pwcn_78@163.com>
2016-08-15 16:33:38 +0800

    个人计划查询gridview开发

---
+++ plan-parent/personalplan-parent/schedule-plan-personalplan-extjs/src/main/webapp/personalplansearch/view/PersonalPlanSearchGrid.js
@@ -0,0 +1,10 @@
+/**
+ * 个人计划表格
+ * @author
+ * @version 1.0.0
+ */
+Ext.define('kalix.plan.personalplan.view.PersonalPlanSearchGrid', {
+    extend: 'kalix.view.components.common.BaseGrid',
+    alias: 'widget.personalplansearchGrid',
+    xtype: 'personalplansearchGridPanel'
+});
