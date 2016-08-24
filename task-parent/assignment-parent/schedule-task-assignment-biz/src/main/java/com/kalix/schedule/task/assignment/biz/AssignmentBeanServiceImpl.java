f73776e708cfe060109764cc098cf65dafc0ab8c
zangyanming <zyanming2002@163.com>
2016-08-24 16:52:05 +0800

    删除无用的import包

--- task-parent/assignment-parent/schedule-task-assignment-biz/src/main/java/com/kalix/schedule/task/assignment/biz/AssignmentBeanServiceImpl.java
+++ task-parent/assignment-parent/schedule-task-assignment-biz/src/main/java/com/kalix/schedule/task/assignment/biz/AssignmentBeanServiceImpl.java
@@ -20,7 +20,6 @@ import com.kalix.schedule.task.assignment.entities.ProgressBean;
 import com.kalix.schedule.task.assignment.entities.ReadingBean;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
