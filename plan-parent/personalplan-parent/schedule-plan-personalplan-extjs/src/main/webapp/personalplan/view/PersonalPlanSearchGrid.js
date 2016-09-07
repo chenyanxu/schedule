f91b7cec30878d6aaaa2febbc68c8c6448458a0d
paning <pwcn_78@163.com>
2016-09-07 10:06:42 +0800

    修改个人计划、工作汇报、工作总结权限控制

--- plan-parent/personalplan-parent/schedule-plan-personalplan-extjs/src/main/webapp/personalplan/view/PersonalPlanSearchGrid.js
+++ plan-parent/personalplan-parent/schedule-plan-personalplan-extjs/src/main/webapp/personalplan/view/PersonalPlanSearchGrid.js
@@ -107,13 +107,13 @@ Ext.define('kalix.plan.personalplan.view.PersonalPlanSearchGrid', {
             items: [
                 {
                     iconCls: "iconfont icon-view-column",
-                    permission: '',
+                    permission: 'view',
                     tooltip: '查看',
                     handler: 'onView'
                 },
                 {
                     iconCls: 'iconfont icon-attachment-column',
-                    permission: '',
+                    permission: 'attachement',
                     tooltip: '附件管理',
                     handler: 'onAttachmentManage'
                 }
--- plan-parent/worksummary-parent/schedule-plan-worksummary-extjs/src/main/webapp/worksummary/view/WorkSummaryGrid.js
+++ plan-parent/worksummary-parent/schedule-plan-worksummary-extjs/src/main/webapp/worksummary/view/WorkSummaryGrid.js
@@ -102,26 +102,26 @@ Ext.define('kalix.plan.worksummary.view.WorkSummaryGrid', {
             items: [
                 {
                     iconCls: "iconfont icon-view-column",
-                    permission: '',
+                    permission: 'view',
                     tooltip: '查看',
                     handler: 'onView'
                 },
                 {
                     //bind: {icon: ''},
                     iconCls: "iconfont icon-edit-column",
-                    permission: '',
+                    permission: 'edit',
                     tooltip: '编辑',
                     handler: 'onEdit'
                 },
                 {
                     iconCls: "iconfont icon-delete",
-                    permission: '',
+                    permission: 'delete',
                     tooltip: '删除',
                     handler: 'onDelete'
                 },
                 {
                     iconCls: 'iconfont icon-attachment-column',
-                    permission: '',
+                    permission: 'attachement',
                     tooltip: '附件管理',
                     handler: 'onAttachmentManage'
                 }
@@ -135,7 +135,7 @@ Ext.define('kalix.plan.worksummary.view.WorkSummaryGrid', {
                 text: '添加',
                 xtype: 'button',
                 iconCls: 'iconfont icon-add',
-                permission: '',
+                permission: 'add',
                 handler: 'onAdd'
             }
         ]
