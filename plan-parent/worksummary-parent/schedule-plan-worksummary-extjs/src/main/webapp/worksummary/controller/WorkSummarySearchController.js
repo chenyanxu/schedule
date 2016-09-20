0ee13c077b857e7f769755b423f683bfb68a37c5
paning <pwcn_78@163.com>
2016-09-20 16:31:57 +0800

    添加重置按钮对特殊字段的支持

--- plan-parent/worksummary-parent/schedule-plan-worksummary-extjs/src/main/webapp/worksummary/controller/WorkSummarySearchController.js
+++ plan-parent/worksummary-parent/schedule-plan-worksummary-extjs/src/main/webapp/worksummary/controller/WorkSummarySearchController.js
@@ -7,7 +7,9 @@ Ext.define('kalix.plan.worksummary.controller.WorkSummarySearchController', {
     onItemClick: function (view, record, item, index, e) {
         var searchForm = this.getView().items.getAt(1).items.getAt(0);
         var orgCode = searchForm.items.getAt(2);
+        orgCode.originalValue = record.data.code;
         orgCode.setValue(record.data.code);
+
         var grid = this.getView().items.getAt(1).items.getAt(1);
         grid.store.reload();
     },
@@ -16,6 +18,7 @@ Ext.define('kalix.plan.worksummary.controller.WorkSummarySearchController', {
         var orgCode = searchForm.items.getAt(2);
         if (nodes.data.length > 0) {
             var node = nodes.data.items[0];
+            orgCode.originalValue = node.data.code;
             orgCode.setValue(node.data.code);
 
             var tree = this.getView().items.getAt(0).items.getAt(1);
